9205ad3f70062471bb5f9ef0d488249993aa3514
pavlidisg <gpavlidis14@gmail.com>
2025-04-09 18:36:49 +0300

    Update Sqlfordb.sql

--- Sqlfordb.sql
+++ Sqlfordb.sql
@@ -4,7 +4,7 @@ varchar(6),date varchar(30),verify varchar(4) DEFAULT 'no', PRIMARY KEY (id));
 CREATE TABLE comments (id int AUTO_INCREMENT,name varchar(20),email varchar(100),msg varchar(100),timestamp
 varchar(100),likes INT NOT NULL DEFAULT '0' ,dislikes INT NOT NULL DEFAULT '0',PRIMARY KEY (id));
 
-CREATE TABLE chat (id int AUTO_INCREMENT,apo varchar(20),se varchar(20),message varchar(3000),tme varchar(25), PRIMARY
+CREATE TABLE chat (id int AUTO_INCREMENT,apo varchar(20),se varchar(20),message varchar(3000),time varchar(25), PRIMARY
 KEY (id));
 
 CREATE TABLE rptokens (rpt_id int AUTO_INCREMENT,rptkn varchar(100),valid varchar(4),expdate varchar(50), PRIMARY KEY
